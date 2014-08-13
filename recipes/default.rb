0ccc2d0a6e5de07ac894a164adf01ad073800274
nathenharvey <nharvey@opscode.com>
2014-08-13 14:33:04 -0400

    Initial apache cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,18 @@
 # Cookbook Name:: apache
 # Recipe:: default
 #
-# Copyright (C) 2014 
+# Copyright (C) 2014
 #
-# 
 #
+#
+package "httpd" do
+  action :install
+end
+
+service "httpd" do
+  action [:start, :enable]
+end
+
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+end
