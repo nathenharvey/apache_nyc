25a4b7913b9c5e6e07ab4c15a15d7d7580ef8407
nathenharvey <nharvey@opscode.com>
2014-08-15 12:00:15 -0400

    remove the lions as a default site

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default["apache"]["greeting"] = "Universe"
 default["apache"]["sites"]["clowns"] = { "port" => 80 }
 default["apache"]["sites"]["bears"] = { "port" => 81 }
-default["apache"]["sites"]["lions"] = { "port" => 83 }
