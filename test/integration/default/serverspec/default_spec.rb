ee5b39c73625dce0f2d4d537405b7d3f18eca434
nathenharvey <nharvey@opscode.com>
2014-08-15 12:01:10 -0400

    Fix the serverspec

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,3 +1,13 @@
-describe port(80) do
-  it { should be_listening }
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+
+describe "apache" do
+  it "is listening on port 80" do
+    expect(port 80).to be_listening
+  end
+
+  it "is listening on port 81" do
+    expect(port 81).to be_listening
+  end
 end
